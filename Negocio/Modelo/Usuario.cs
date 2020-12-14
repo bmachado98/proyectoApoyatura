0445cb5a8ec75581020043f06eb1b06af1596310
bmachado98 <74577552+bmachado98@users.noreply.github.com>
2020-12-13 21:44:12 -0300

    primer punto

---
+++ Negocio/Modelo/Usuario.cs
@@ -0,0 +1,63 @@
+﻿using Persistencia.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Negocio.Modelo
+{
+    /// <summary>
+    /// la capa de modelo se encarga de llamar realizar los calculos de la logica.
+    /// por ejemplo
+    /// </summary>
+    public class Usuario
+    {
+        private string nombre;
+        private string pasword;
+
+        public Usuario(string nombre, string pasword)
+        {
+            //
+            if (nombre.Length > 60)
+            {
+                this.nombre = nombre.Substring(0,4);
+            }
+            else
+            {
+                this.nombre = nombre;
+            }            
+            this.pasword = pasword;
+        }
+
+
+        
+        public bool Ingresar(string nombreUsuario, string password, string programa)
+        {
+            bool ingreso = false;
+            UsuarioRepo usuarioRepo = new UsuarioRepo();
+            ingreso = usuarioRepo.Ingresar(nombreUsuario, password, programa);
+            return ingreso;
+            //throw new NotImplementedException();
+        }
+        public bool Ingresar(string nombreUsuario, string password)
+        {
+            bool ingreso = false;
+            UsuarioRepo usuarioRepo = new UsuarioRepo();
+            ingreso = usuarioRepo.Ingresar(nombreUsuario, password);
+            return ingreso;
+            //throw new NotImplementedException();
+        }
+
+
+        public bool Ingresar()
+        {
+            bool ingreso = false;
+            UsuarioRepo usuarioRepo = new UsuarioRepo();
+            ingreso = usuarioRepo.Ingresar(this.nombre, this.pasword);
+            return ingreso;
+            //throw new NotImplementedException();
+        }
+
+
+    }
+
+}
---
+++ Presentacion/Formularios/Login.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Negocio.Modelo;
+using Presentacion.Helpers;
+
+namespace Presentacion.Formularios
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            //guardar los mensajes de error
+            string errores = "";
+            bool errorBool = false;
+            string nombreUsuario = txtuser.Text.Trim();
+            string password = txtpassword.Text.Trim();
+            if (string.IsNullOrEmpty(nombreUsuario)){
+                errores += "ingrese el nombre de usuario\n";
+                errorBool = true;
+            }
+            if (string.IsNullOrEmpty(password)){
+                errores += "ingrese el pasword\n";
+                errorBool = true;
+            }
+            if (!errorBool)
+            {
+                //llamaria a login
+                Usuario usr = new Usuario(nombreUsuario, password);//
+                //usr.Ingresar(nombreUsuario, password, Variables.programa);
+                bool ingreso = usr.Ingresar(nombreUsuario, password, Variables.programa);
+                if (ingreso== true)
+                {
+                    FormInicio p = new FormInicio();
+                    p.Show();
+                    //p.ShowDialog();
+                }
+                else
+                {
+                    string title = "Close Window";                    
+                    MessageBoxButtons buttons = MessageBoxButtons.OK;
+                    DialogResult result = MessageBox.Show("password incorrecto", title, buttons, MessageBoxIcon.Warning);
+                }              
+            }
+            else
+            {
+                //muestro lo errores      
+                //string message = "Do you want to abort this operation?";
+                string title = "Close Window";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result = MessageBox.Show(errores, title, buttons, MessageBoxIcon.Warning);
+
+            }
+            //Usuario usr = new Negocio.Modelo.Usuario();
+            
+            
+            /*if (checkrecordar.Checked == true)
+            {
+                Properties.Settings.Default.UserName = txtuser.Text;
+                Properties.Settings.Default.password = txtpassword.Text;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                Properties.Settings.Default.UserName = "";
+                Properties.Settings.Default.password = "";
+                Properties.Settings.Default.Save();
+            }*/
+
+            
+            
+            //guardo los datos
+            
+
+        }
+
+        private void btnChat_Click(object sender, EventArgs e)
+        {
+            Chat chat1 = new Chat(1, 1);
+            Chat chat2 = new Chat(3, 1);
+            chat1.Show();
+            chat2.Show();
+        }
+    }
+}
